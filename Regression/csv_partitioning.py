50feb06cbf1742d5e1de131ed1401b4a0b641d90
yasin rezaei <yasinrezaei@hotmail.com>
2023-12-06 12:09:17 +0330

    start learning ml

--- Regression/csv_partitioning.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-# Reading the first 200 rows from the large CSV file
-df = pd.read_csv('dataset/heart_attack_prediction_dataset.csv', nrows=200)
-
-# Saving the 200 rows to another CSV file
-df.to_csv('dataset/dataset.csv', index=False)
